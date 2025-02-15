b95fd6df5a8f1baf5e113458442d237d73e325e4
Tuấngg Anhhh <macofanh@Tuongg-Anhh.local>
2025-02-15 19:41:58 +0700

    nav

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 let isHidden = true
+document.title = 'Navbar Notifi'
 const ele = document.getElementById('btn')
 const nextEle = ele.nextElementSibling
 ele.addEventListener('click', () => {
@@ -6,11 +7,27 @@ ele.addEventListener('click', () => {
         ele.parentElement.classList.add('par')
         isHidden = false
         nextEle.classList.add('show')
-        ele.textContent = 'Click me again'
+        ele.classList.add('rotate-[180deg]')
     } else {
         ele.parentElement.classList.remove('par')
         isHidden = true
         nextEle.classList.remove('show')
-        ele.textContent = 'Click me'
+        ele.classList.remove('rotate-[180deg]')
+    }
+})
+
+const ele2 = document.getElementById('icon')
+const check = true
+const nextEle2 = ele2.nextElementSibling
+const nextEle3 = nextEle2.nextElementSibling
+ele2.addEventListener('click', () => {
+    nextEle2.classList.add('tran2')
+    nextEle3.classList.add('tran3')
+})
+
+document.addEventListener('click', (e) => {
+    if (e.target.classList.contains('tran3')) {
+        nextEle2.classList.remove('tran2')
+        nextEle3.classList.remove('tran3')
     }
 })
